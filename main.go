14b97096a778da4ae6e03a641665a9d1208b7f14
マリウス <marius@xn--gckvb8fzb.com>
2024-02-10 19:45:27 -0500

    Fixed command issuing, added attach skipping

--- main.go
+++ main.go
@@ -12,12 +12,21 @@ type Device struct {
 	ProductID int
 }
 
+var INTEGRATED_KEYBOARDS = []string{
+	"1:1:AT_Translated_Set_2_keyboard",
+	"4012:2782:keyd_virtual_keyboard",
+}
+
 var KEYBOARDS = map[string]Device{
 	"rama-m60a": Device{VendorID: 0x5241, ProductID: 0x060a},
 	"rama-kara": Device{VendorID: 0x5241, ProductID: 0x4b52},
 }
 
+var ATTACHED_KEYBOARDS map[string]Device
+
 func main() {
+	ATTACHED_KEYBOARDS = make(map[string]Device)
+
 	listener, _ := hotplug.New(
 		hotplug.DevIfHid,
 		func(devIf *hotplug.DeviceInterface) {
@@ -50,6 +59,11 @@ func main() {
 
 			for name, device := range KEYBOARDS {
 				if device.VendorID == vendorId && device.ProductID == productId {
+					if _, isAttached := ATTACHED_KEYBOARDS[name]; isAttached {
+						log.Printf("'%s' already attached, skipping\n", name)
+						continue
+					}
+
 					log.Printf(
 						"Attached '%s' bus=%d address=%d vid=%04x pid=%04x dev=%s\n",
 						name, busNumber, address, vendorId, productId, devIf.Path,
@@ -62,12 +76,15 @@ func main() {
 						)
 
 						disableIntegratedKeyboard(false)
+						delete(ATTACHED_KEYBOARDS, name)
 					})
 					if err != nil {
 						log.Println(err)
+						continue
 					}
 
 					disableIntegratedKeyboard(true)
+					ATTACHED_KEYBOARDS[name] = device
 				}
 			}
 
@@ -90,12 +107,10 @@ func disableIntegratedKeyboard(disable bool) {
 		status = "disabled"
 	}
 
-	cmd = exec.Command("swaymsg", "input",
-		"1:1:AT_Translated_Set_2_keyboard",
-		"events", status)
-	cmd.Run()
-	cmd = exec.Command("swaymsg", "input",
-		"4012:2782:keyd_virtual_keyboard",
-		"events", status)
-	cmd.Run()
+	for _, ikbd := range INTEGRATED_KEYBOARDS {
+		cmd = exec.Command("swaymsg", "input",
+			ikbd,
+			"events", status)
+		cmd.Run()
+	}
 }
